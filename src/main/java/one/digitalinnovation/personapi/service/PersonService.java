a2d219d42a82dfdd2a90720793e2bcba986dbeba
Davi dos Santos <davi.slima75@gmail.com>
2021-08-10 15:45:30 -0300

    Developed get person by id

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -4,11 +4,13 @@ import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entities.Person;
 import one.digitalinnovation.personapi.dto.mapper.PersonMapper;
+import one.digitalinnovation.personapi.exceptions.PersonNotFoundException;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -40,5 +42,12 @@ public class PersonService {
                 .map(personMapper::toDto)
                 .collect(Collectors.toList());
 
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+       Person person = personRepository.findById(id)
+               .orElseThrow(() -> new PersonNotFoundException(id));
+
+       return personMapper.toDto(person);
     }
 }
